d6f78f4f632c60c3e5371b18bcca6f2029aacaf3
dgruncharov <damian.grancharov@gmail.com>
2015-12-07 19:22:39 +0200

    TeamController implemented

---
+++ HRMS/HRMS/Models/ViewModels/TeamViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CMS.DocumentEngine.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.Models.ViewModels
+{
+    public class TeamViewModel
+    {
+        public Team Team { get; set; }
+        public List<Employee> Members { get; set; }
+    }
+}
