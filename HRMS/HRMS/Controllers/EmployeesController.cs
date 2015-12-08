97402e5fc3bddc1c6c0afcf6a09cee224cdeac78
Iliyan Ramov <iramov91@gmail.com>
2015-12-08 22:35:49 +0200

    Sait navigation updated

--- HRMS/HRMS/Controllers/EmployeesController.cs
+++ HRMS/HRMS/Controllers/EmployeesController.cs
@@ -14,12 +14,17 @@
         // GET: Employees
         public ActionResult Index()
         {
-            //Getting all the sections with employees who are not assigned into teams
-            var employeeSections = FreeEmployeesProvider.GetFreeEmployees();
-            //employeeSections = null;
-            return View(employeeSections);
+            //Getting all employees in the site and printing them as a table
+            var allEmployees = EmployeeProvider.GetEmployees();
+            return View(allEmployees);
         }
 
+        /// <summary>
+        /// Finding section of employees by input param NodeId, getting its children and printing them in a table
+        /// </summary>
+        /// <param name="id">The NodeId of a Section with employees</param>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult SectionDetails(int? id)
         {
             if (id == null)
@@ -31,13 +36,15 @@
             {
                 return HttpNotFound();
             }
+            //ViewModel to store the data of the Parent Section and its children Employees
             var viewModel = new EmployeeSectionsWithChilds();
             var allChildren = new List<Employee>();
-            var employees = sectionEmployees.Children;
+            //Getting all the children on a section, saving them in a collection and printing them with they parent section name in a table
+            var sectionChildren = sectionEmployees.Children;
             viewModel.Section = sectionEmployees;
-            if (employees != null)
+            if (sectionChildren != null)
             {
-                foreach (var child in employees)
+                foreach (var child in sectionChildren)
                 {
                     var childToAdd = EmployeeProvider.GetEmployee(child.NodeID, "en-Us", "HRMS");
                     allChildren.Add(childToAdd);
@@ -48,7 +55,13 @@
             return View(viewModel);
         }
 
-        public ActionResult EmployeeDetails(int? id)
+        /// <summary>
+        /// Finding employee by input param id and printing all its fields
+        /// </summary>
+        /// <param name="id">The NodeId for the searched employee</param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult EmployeeDetails(int? id,int? parentId, int? employeeSectionId)
         {
             if (id == null)
             {
@@ -59,7 +72,28 @@
             {
                 return HttpNotFound();
             }
+            if (parentId != null)
+            {
+                ViewBag.ParentId = parentId;
+            }
+            if (employeeSectionId != null)
+            {
+                ViewBag.EmployeeSectionId = employeeSectionId;
+            }
             return View(employee);
+        }
+
+        /// <summary>
+        /// Printing all the sections with free employees
+        /// </summary>
+        /// <returns>List of sections with employees</returns>
+        [HttpGet]
+        public ActionResult AllSections()
+        {
+            //Getting all the sections with employees who are not assigned into teams
+            var employeeSections = FreeEmployeesProvider.GetFreeEmployees();
+            //employeeSections = null;
+            return View(employeeSections);
         }
 
     }
